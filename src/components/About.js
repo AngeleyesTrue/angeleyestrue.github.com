852180f4240915b3b5b81025f560604a1ae09b19
Angeleyes <angeleyes.true@gmail.com>
2018-07-23 15:23:26 +0900

    Navigation 적용

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const About = () => {
+    return (
+        <div>
+            <p>Angeleyes</p>
+            <p>
+                <Link to="/">Navigate to Home Page!</Link>
+            </p>
+        </div>
+    );
+};
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
 
 const App = () => {
     return (
-        <div>
-            <p>Hello everyone!</p>
-            <p>
-                Goood!!~
-            </p>
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/About" component={About} />
+                </Switch>
+            </div>
+        </Router>
     );
 };
 
