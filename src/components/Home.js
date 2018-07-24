03442ffcfb144e6e596a39f402dfa4c917209683
Angeleyes <angeleyes.true@gmail.com>
2018-07-24 13:02:36 +0900

    Hyperlink test

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,10 @@ const Home = () => {
             <p>
                 <Link to="/About">Navigate to Abuout Page</Link>
             </p>
+            <p>
+                <a href="http://naver.com" target="_blank">네이버</a><br />
+                <a href="javascript:window.open('http://naver.com')">자바스크립트 오픈</a>
+            </p>
         </div>
     );
 };
