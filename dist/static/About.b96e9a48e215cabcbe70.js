afb5c18e2ac28b90aa42511e8f8adce2ef49ac2c
Angeleyes <angeleyes.true@gmail.com>
2019-08-26 17:08:30 +0900

    Package Update 및 New Build

--- dist/static/About.b96e9a48e215cabcbe70.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{237:function(e,n,t){"use strict";t.r(n);var a=t(1),l=t.n(a),c=t(510);n.default=function(){return l.a.createElement("div",null,l.a.createElement("p",null,"Angeleyes"),l.a.createElement("p",null,l.a.createElement(c.a,{to:"/"},"Navigate to Home Page!")))}}}]);
-//# sourceMappingURL=About.b96e9a48e215cabcbe70.js.map
